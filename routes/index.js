3bfb34c163fed30c61f2087d741667342dbed567
Xlemon <575590793@qq.com>
2019-03-30 19:00:05 +0800

    封装查询sql、查询热点接口完毕

--- routes/index.js
+++ routes/index.js
@@ -1,54 +1,77 @@
 const router = require('koa-router')()
 const pool = require('../db/pool/pool.js');
-router.post('/saveHotSpot.do',(ctx, next) => {
+const {success,failure} = require('../lib/responseResult');
+const {query} = require('../lib/query')
+router.post('/saveHotSpot.do',async (ctx, next) => {
   //let connection = pool.getConnection();
     let body = ctx.request.body;
-    console.log(body);
     let {data} = body;
     if(!data){
       ctx.body = {
         code : 500,
         status : false,
         message : "data不可以为空"
-      }
+      };
       return
     }
-    next()
+    await next()
 }, async (ctx,next) => {
-    let hasError = false;
+    let res = {};
     try {
+        let sql = "insert into HOT_INFORMATION set Title=?,Link=?,Platform=?,InfoTime=?;";
         let {data} = ctx.request.body;
         data = typeof data === 'string' ? JSON.parse(data) : data;
-        data.forEach((item) => {
-            const {title, link, platform} = item;
+        for (let item of data) {
+            const {title = '', link = '', platform = ''} = item;
             let infoTime = new Date().getTime();
-            pool.getConnection(function (err, connection) {
-                connection.query("insert into HOT_INFORMATION set Title=?,Link=?,Platform=?,InfoTime=?;", [title, link, platform, infoTime], function (err, data) {
-                    if (err) {
-                        console.log(err);
-                        hasError = true;
-                    }
-                })
+            await query(sql, [title, link, platform, infoTime]).then(function (result) {
+                console.log(result);
+                res = success(result);
+                ctx.result = res;
+                next()
             });
-        });
-  }catch (e){
-      console.log(e);
-      hasError = true;
-  }
-    if(hasError) {
-        ctx.body = {
-            code : 500,
-            status : false,
-            message : "系统繁忙"
         }
-    }else{
-        ctx.body = {
-            code : 200,
-            status : true,
-            message : "上传成功"
-        }
+    }catch (e){
+        res = failure(e);
+        ctx.result = res;
+        next()
     }
+},async (ctx,next)=>{
+    ctx.body = ctx.result;
 });
 
 
+
+router.get('/getHotSpot.do',async (ctx,next) => {
+    let res = {};
+    let param = ctx.request.query;
+    const {title,link,platform} = param;
+    try {
+        //let sql = `select * from HOT_INFORMATION where ${title ? 'Title=?,' : ''}${link ? 'Link=?,' : ''}${platform ? 'Platform=?' : ''};limit 50`;
+        let filterString = '';
+        if(title != null){
+            filterString+='Title=?';
+        }
+        if(link != null){
+            filterString && (filterString+=',')
+            filterString += 'Link=?'
+        }
+        if(platform != null){
+            filterString && (filterString+=',')
+            filterString += 'platform=?'
+        }
+        let sql = `select * from HOT_INFORMATION ${filterString ? `where ${filterString}` : ''} limit 50`;
+        let result = await query(sql,[title,link,platform]).then(queryResult=>{
+            return queryResult
+        }).catch(err=>{
+            res = failure(err);
+        });
+        if(result){
+            res = success({data : result});
+        }
+    }catch (e){
+        res = failure(e);
+    }
+    ctx.body = res;
+});
 module.exports = router
